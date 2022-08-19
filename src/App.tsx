b1e071261d155bd1a44ef493ad9818515308f730
ElianMD <elian@nikolovdzn.com>
2022-08-19 15:45:54 +0300

    Delete

--- src/App.tsx
+++ src/App.tsx
@@ -7,38 +7,6 @@ import ComeAgain from "./ComeAgain";
 import Congratulations from "./Congratulations";
 import Form from "./Form";
 export default function App(): JSX.Element {
-  const greetings = people.names.map((person) => {
-    return (
-      <Greeting
-        key={person.name}
-        name={person.name}
-        punctuation={person.punctuation}
-        age={person.age}
-        gender={person.gender}
-      />
-    );
-  });
-  const farewell = people.names.map((person) => {
-    return (
-      <Farewell
-        key={person.name}
-        name={person.name}
-        age={person.age}
-        gender={person.gender}
-      />
-    );
-  });
-  const comeagain = people.names.map((person) => {
-    return (
-      <ComeAgain
-        key={person.name}
-        name={person.name}
-        gender={person.gender}
-        age={person.age}
-      />
-    );
-  });
-
   const [count, setCount] = useState(0);
   function increase() {
     setCount((currCount) => currCount + 1);
@@ -48,13 +16,6 @@ export default function App(): JSX.Element {
   function pages() {
     setPage((currPage) => currPage + 1);
   }
-  const links = people.names.map((person) => {
-    return (
-      <p key={person.name}>
-        <Link to={`/congratulations/${person.name}`}>{person.name}</Link>;
-      </p>
-    );
-  });
   return (
     <>
       <h1>Simple</h1>
@@ -68,23 +29,8 @@ export default function App(): JSX.Element {
       <p>
         <Link to="/">Home!</Link>
       </p>
-      <p>
-        <Link to="/comeagain">Come Again!</Link>
-      </p>
-      <p>
-        <Link to="/farewell">Farewell!</Link>
-      </p>
-      {links}
-      <Link to="/greetings">Greetings!</Link>
       <Routes>
         <Route path="/" />
-        <Route path="/greetings" element={greetings} />
-        <Route path="/comeagain" element={comeagain} />
-        <Route path="/farewell" element={farewell} />
-        <Route
-          path="/congratulations/:name2"
-          element={<Congratulations />}
-        ></Route>
       </Routes>
     </>
   );
